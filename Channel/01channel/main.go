a51574353bb35de6cccfd188b7ebf648110e3c0c
yesemenglong <1981376124@qq.com>
2021-04-07 16:23:25 +0800

    channel 学习

--- Channel/01channel/main.go
+++ Channel/01channel/main.go
@@ -8,8 +8,12 @@ func main() {
 	c := make(chan int, 2)
 	c <- 1
 	c <- 2
-
+	close(c)
+	fmt.Println(<-c)
+	fmt.Println(<-c)
 	fmt.Println(<-c)
 	fmt.Println(<-c)
 	fmt.Println(cap(c))
 }
+
+// 把一个有缓冲的channel 关闭后，再读取channel 的值都会是 0
