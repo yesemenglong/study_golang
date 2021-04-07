0812e43860e17669f67f7a198dd79c5dc02fa907
yesemenglong <1981376124@qq.com>
2021-04-07 18:12:28 +0800

    string 转 slice

--- unsafe/03unsafe/main.go
+++ unsafe/03unsafe/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"reflect"
+	"unsafe"
+)
+
+// string 转 slice
+func main() {
+	var s = "sss"
+	b := []byte(s)
+	fmt.Println(bytes2string(b))
+}
+
+func bytes2string(b []byte) string {
+	sliceHeader := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+
+	sh := reflect.StringHeader{
+		Data: sliceHeader.Data,
+		Len:  sliceHeader.Len,
+	}
+	return *(*string)(unsafe.Pointer(&sh))
+}
